3b55d91f80d0694d1368402f73deb7a092072fed
sament <foundation.samiksha@gmail.com>
2025-06-25 00:25:28 +0530

    khana

--- gcp-terraform/terraform.tfvars
+++ gcp-terraform/terraform.tfvars
@@ -1,1 +1,5 @@
-admin_password   = "YourSecurePassword123!"
+location = "East US"    
+vm_count = 1
+admin_username = "avdadmin" 
+admin_password = "AdminPass123!"
+# Note: Ensure that the password meets Azure's complexity requirements.
--- gcp-terraform/variavles.tf
+++ gcp-terraform/variavles.tf
@@ -1,49 +1,12 @@
-variable "client_id" {
-  type        = string
-  description = "AZURE_CLIENT_ID"
-}
-
-variable "client_secret" {
-  type        = string
-  description = "AZURE_CLIENT_SECRET"
-}
-
-variable "tenant_id" {
-  type        = string
-  description = "AZURE_TENANT_ID"
-}
-
-variable "subscription_id" {
-  type        = string
-  description = "AZURE_SUBSCRIPTION_ID"
-}
-
-variable "location" {
-  type        = string
-  default     = "East US"
-  description = "Azure region"
-}
-
-variable "project_id" {
-  type        = string
-  default     = "avdproj"
-  description = "Project name prefix"
-}
-
+variable "location" {}
+variable "project_id" {}
+variable "vm_count" {}
 variable "admin_username" {
+  description = "Username for the AVD admin account"
   type        = string
-  default     = "azureadmin"
-  description = "Admin username for VM"
 }
-
 variable "admin_password" {
+  description = "Password for the AVD admin account"
   type        = string
-  description = "Admin password for VM"
   sensitive   = true
-}
-
-variable "vm_count" {
-  type        = number
-  default     = 1
-  description = "Number of VMs to create"
 }
