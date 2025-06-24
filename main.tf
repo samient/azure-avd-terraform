c8ca3bb42e49755e01d5fedbb542e6ed8e4cf99f
sament <foundation.samiksha@gmail.com>
2025-06-24 22:05:13 +0530

    main

--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.project}-vnet"
+  name                = "${var.project}-vnet01"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "${var.project}-subnet"
+  name                 = "${var.project}-subnet01"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
